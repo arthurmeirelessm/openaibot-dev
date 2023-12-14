d3d22405b6964afec71b6fdb46a24d2786666a3b
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2023-12-14 03:06:11 -0300

    .env chnge

--- src/bots/explore_world.py
+++ src/bots/explore_world.py
@@ -3,6 +3,9 @@ import requests
 from luis.luis_client import LUIS
 from openAI.client import GPT3ChatClient
 from speech.client import Speech
+from bots.finalization import Finalization
+import re
+from orchestrator import TravelAssistant
 
 
 class Explore_world:
@@ -10,6 +13,8 @@ class Explore_world:
         self.luis = LUIS()
         self.openAI = GPT3ChatClient()
         self.speech = Speech()
+        self.finalization = Finalization()
+        self.travelAssistant = TravelAssistant
 
     def introduction(self):
         print(
@@ -24,10 +29,12 @@ class Explore_world:
         
         luis_intent = self.luis.analyze_language(user_input)
         
-        intent_actions = {"exploretheworld": lambda: self.explore_culture(user_input)}
+        intent_actions = {"gotoout": self.finalization.introduction, "exploretheworld": lambda: self.explore_culture(user_input)}
         intent_actions.get(luis_intent, self.handle_unrecognized_intent)()
-
+        more_questions = self.ask_more_questions(luis_intent)
             
+        
+        
     def handle_unrecognized_intent(self):
         print("Desculpe, não consegui identificar sua intenção. 🫤\nMe pergunte ou me conte uma curiosidade de algum lugar do mundo.")
         self.call_speech()
@@ -35,3 +42,16 @@ class Explore_world:
     def explore_culture(self, user_input):
         print("passei")
         self.openAI.generate_response(user_input)
+        
+    
+ 
+    def ask_more_questions(self, luis_intent):
+        if luis_intent == "exploretheworld":
+            print("1 - Sim\n2 - Não\n3 - Voltar a opção inicial")
+            user_input = input("You: ")
+            if re.search(r'\b(sim|claro|quero)\b', user_input, flags=re.IGNORECASE):
+                self.call_speech()
+            elif re.search(r'\b(n[ãa]o|nopes|nada)\b', user_input, flags=re.IGNORECASE):
+                self.finalization.introduction()
+            else:
+                self.travelAssistant.start_conversation()
